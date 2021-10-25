fbd7a4539709c8e5516d556c04cc84fd7f857449
Ana Clara <clarafag@gmail.com>
2021-10-25 12:56:36 -0300

    Adicionadas atividades de variáveis

---
+++ CirculoAreaPerimetro/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    /*Exercício 2
+
+1 - Crie um projeto no IntelliJ com o nome CirculoAreaPerimetro
+2 - Crie um código que pede para o usuário digitar o diâmetro de um círculo (que será uma variável do tipo Int) e calcule a área e o perímetro (que serão Doubles).
+4 - Faça uma conversão de tipos (de Int para Double) quando for calcular os resultados*/
+
+    var diametro = 0
+    var area = 0.00
+    var perimetro = 0.00
+    var pi = 3.14
+    var raio = 0.00
+    var leitura = Scanner(System.`in`)
+
+    print("Digite o valor do diametro da circunferência: ")
+    diametro = leitura.nextInt()
+
+    perimetro = diametro * pi
+
+    raio = diametro.toDouble() / 2
+    area = pi * raio * raio
+
+    print("O perimêtro do circulo é $perimetro e a área é $area")
+
+
+}
---
+++ NomeandoVariaveis/src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    /*Exercício 1
+
+    1 - Crie um novo projeto chamado NomeandoVariaveis
+    2 - Crie variáveis que armazenem o nome do usuário, idade, preferência de emprego, nacionalidade e tipo sanguíneo
+    3 - Pense como você pode nomear essas variáveis para ficarem fáceis de serem identificadas
+    4 - Use a idade do usuário para calcular o ano de seu nascimento, usando o ano atual como base (2021)
+    5 - No fim, printe todos os valores no console, juntamente com o ano em que o usuário nasceu */
+
+    var nome = ""
+    var idade = 0
+    var empregoPref = ""
+    var nacionalidade = ""
+    var tipoSang = ""
+    var leitura = Scanner(System.`in`)
+
+
+    print("Digite seu nome: ")
+    nome = leitura.next()
+
+    print("Qual a sua idade? ")
+    idade = leitura.nextInt()
+
+    print("Qual sua preferência de emprego? ")
+    empregoPref =  leitura.next()
+
+    print("Qual sua nacionalidade? ")
+    nacionalidade = leitura.next()
+
+    print("Qual seu tipo sanguíneo? ")
+    tipoSang = leitura.next()
+
+    var anoDeNasc = 0
+
+    anoDeNasc = 2021 - idade
+    println("Você nasceu no ano $anoDeNasc")
+}
